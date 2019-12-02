68560646aa9da4c53caea48a502b6cf38b057b0c
Vision-X <michael.s.marlow@gmail.com>
2019-12-02 10:30:41 -0700

    create some routes, sending responses to routes, bring in pg-monitor for performance logging

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-let http    = require('http');
-let express = require('express');
-let app     = express();
-let connStr = "postgres://localhost:5432/massive-test";
-let massive = require('massive');
+const http    = require('http');
+const express = require('express');
+const app     = express();
+const massive = require('massive');
+const monitor = require('pg-monitor');
+const connStr = "postgres://localhost:5432/massive-test";
 
 let db;
 
@@ -13,35 +14,38 @@ async function dbInstance() {
     database: 'massive-test',
     ssl: false
   });
-}
+};
 
 app.set('db', dbInstance());
 
-app.get('/', async (req, res) => {
-  const tables = await db.listTables();
-  console.log("  T   A   B   L   E   S   ", tables);
+app.get('/newUser/:email/:first/:last', async (req, res) => {
+  let mail = req.params.email;
+  let fName = req.params.first;
+  let lName = req.params.last;
   let newUser = {
-    email: "Tarful@Wookie.com",
-    first: "Tarful",
-    last: "N/A"
+    email: mail || "Tarful@Wookie.com",
+    first: fName || "Tarful",
+    last: lName || "N/A"
   };
 
   db.users.save(newUser, function(err, result) {
     if (err) console.log(err)
-    else {
-      console.log("New User :", result);
-      res.send("New User Added!");
-    }
+    else console.log("New User : ", result)
+  }).then(() => {
+    res.send("New User Added!!!!");
   });
-
 });
 
 app.get('/users', async (req, res) => {
-  const users = await db.users.find({
-    'id >': 0
-  });
+  const users = await db.query('select * from users');
   console.log("  U   S   E   R   S   ", users);
-  res.send(users)
+  res.send(users);
+});
+
+app.get('/user/:id', async (req, res) => {
+  let userId = req.params.id;
+  const user = await db.users.find({ 'id =': userId });
+  user.length ? res.send(user) : res.send("User record not found");
 });
 
 http.createServer(app).listen(8080);
